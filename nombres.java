4d52e5c5a9b6aba23da3e5b3aefced06db8cffcd
unknown <yaelmexmontero@gmail.com>
2024-02-27 09:59:13 -0600

    Ahora este imprime los nombres de mis compañeros

--- nombres.java
+++ nombres.java
@@ -3,6 +3,8 @@ public class nombres {
 
 	public static void main(String[] args) {
 		System.out.println("Mi nombre es: José Yael Mex Montero");
+		
+		System.out.println("Ahora los nombres de mis compañeros son: Vianey y Jose Ac");
 
 	}
 
